2087c0b50886518d51988b47443650224ab5fcbc
avb <avaniabhattacharya@gmail.com>
2026-02-27 20:41:15 -0800

    temp edits

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ ez::Drive chassis(
 
 ez::tracking_wheel horiz_tracker(
     1,     // Port
-    2,     // Wheel Diameter
+    2,     // Wheel Diameter. 
     4.0);  // Distance to center of robot
 
 Robot robot(
@@ -23,10 +23,10 @@ Robot robot(
     'C',  // matchloaderPort
     'D'   // wingPort
 );
-
+bool lever_up = false;
 bool drive_arcade = false;
 bool was_outtaking = false;
-
+bool isDown = false;
 void drive_mode_task() {
   while (true) {
     if (master.get_digital_new_press(DIGITAL_UP)) {
@@ -42,10 +42,6 @@ void initialize() {
   ez::ez_template_print();
 
   pros::delay(500);
-
-  // Look at your horizontal tracking wheel and decide if it's in front of the midline of your robot or behind it
-  //  - change `back` to `front` if the tracking wheel is in front of the midline
-  //  - ignore this if you aren't using a horizontal tracker
   chassis.odom_tracker_back_set(&horiz_tracker);
 
   chassis.opcontrol_curve_buttons_toggle(true);
@@ -54,20 +50,6 @@ void initialize() {
   default_constants();
 
   ez::as::auton_selector.autons_add({
-      {"Drive\n\nDrive forward and come back", drive_example},
-      {"Turn\n\nTurn 3 times.", turn_example},
-      {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
-      {"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
-      {"Swing Turn\n\nSwing in an 'S' curve", swing_example},
-      {"Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining},
-      {"Combine all 3 movements", combining_movements},
-      {"Interference\n\nAfter driving forward, robot performs differently if interfered or not", interfered_example},
-      {"Simple Odom\n\nThis is the same as the drive example, but it uses odom instead!", odom_drive_example},
-      {"Pure Pursuit\n\nGo to (0, 30) and pass through (6, 10) on the way.  Come back to (0, 0)", odom_pure_pursuit_example},
-      {"Pure Pursuit Wait Until\n\nGo to (24, 24) but start running an intake once the robot passes (12, 24)", odom_pure_pursuit_wait_until_example},
-      {"Boomerang\n\nGo to (0, 24, 45) then come back to (0, 0, 0)", odom_boomerang_example},
-      {"Boomerang Pure Pursuit\n\nGo to (0, 24, 45) on the way to (24, 24) then come back to (0, 0, 0)", odom_boomerang_injected_pure_pursuit_example},
-      {"Measure Offsets\n\nThis will turn the robot a bunch of times and calculate your offsets for your tracking wheels.", measure_offsets},
   });
 
   chassis.initialize();
@@ -81,11 +63,11 @@ void initialize() {
 }
 
 void disabled() {
-  // . . .
+
 }
 
 void competition_initialize() {
-  // . . .
+
 }
 
 void autonomous() {
@@ -187,9 +169,15 @@ void opcontrol() {
     }
 
     // Score
-    if (master.get_digital_new_press(DIGITAL_R2))
-      robot.score();
+// Lever toggle
+  if (master.get_digital_new_press(DIGITAL_R2)) {
+    lever_up = !lever_up;
 
+    if (lever_up)
+      robot.moveLeverTo(110, +1, 127);  
+      pros::delay(1000);  // 
+      robot.moveLeverTo(0, -1, 127);    
+  }
     // Matchloader
     if (master.get_digital_new_press(DIGITAL_DOWN))
       robot.toggleMatchloader();
